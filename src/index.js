7b7762d2d5173c5c413bf07f7406b331bee0cfe6
Semukelo Mathe <semmathe@gmail.com>
2024-08-09 17:33:20 +0200

    app code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+
+const App = () => {
+  const [active1, setActive1] = useState(false);
+
+  const toggleMode1 = () => {
+    setActive1(c => !c);
+  };
+
+  return (
+    <div className="app">
+      <div className={`sec__1 ${active1 ? "light__mode" : ""}`} >
+        <button onClick={toggleMode1}>{active1 ? "Light Mode" : "Dark Mode"}</button>
+      </div>
+
+      <div className={`sec__2 ${active1 ? "dark__mode" : ""}`}>
+        <button onClick={toggleMode1}>{active1 ? "Dark Mode" : "Light Mode"}</button>
+      </div>
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
